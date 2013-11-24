f494898d19f1f6fd2fdeeeb60aaa0792c9b9cfc8
Marc MacLeod <marbemac@gmail.com>
2013-11-23 18:37:18 -0800

    use replaceState instead of pushState, and use the full server url.
    The full server URL is needed to make DDP work.

--- www/js/meteor-rider.js
+++ www/js/meteor-rider.js
@@ -57,7 +57,7 @@ var MeteorRider = {
         
         // set the window.location object correctly so iron-router 
         // and other packages that depend on window.location work correctly
-        window.history.pushState({}, "", "/");
+        window.history.replaceState({}, "", meteorUrl);
         
         // replace the document with the new document/data
         document.open();
