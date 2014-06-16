b38c3b1d4fd4679b33f6150c120491cab4035730
alan bount <alan@zeroasterisk.com>
2014-06-16 10:26:41 -0400

    meteor/startup.js reload update
    re #17 
    
    `Reload._onMigrate` added as a "newer" method of registering callback on hot code reload
    
    `Meteor._reload.onMigrate` is deprecated (but should still be supported)
    
    https://github.com/meteor/meteor/blob/d477c8d03bb078f7e8e85dbe4b51db7ae5689573/packages/reload/deprecated.js
    
    (also flipped the logic to fail faster, so the function is easier to read)

--- meteor/startup.js
+++ meteor/startup.js
@@ -1,10 +1,28 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    if (typeof phonegapapp !== 'undefined') {// phonegap context
+    if (typeof phonegapapp === 'undefined') {
+      return;
+    }
+    // we have a phonegap app
+    //   attempt to setup a callback handler for hot reloads
+    
+    if (typeof Reload._onMigrate === 'function') {
+      // this is the newer method, internal?
+      Reload._onMigrate('phonegapapp', function () {
+        phonegapapp.meteorRider();
+        return [false];
+      });
+      return;
+    }
+    
+    if (typeof Meteor._reload.onMigrate === 'function') {
+      // this is the older method, deprecated?
       Meteor._reload.onMigrate('phonegapapp', function () {
         phonegapapp.meteorRider();
         return [false];
       });
+      return;
     }
+
   });
 }
