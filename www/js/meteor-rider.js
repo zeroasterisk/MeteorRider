b0131893352be0053cde5ebf51250997721f9863
DanyHunter <dannytenaglias@gmail.com>
2014-06-28 15:53:00 -0500

    meteorUrl can now be referenced inside the onSuccess callback

--- www/js/meteor-rider.js
+++ www/js/meteor-rider.js
@@ -34,17 +34,17 @@ var MeteorRider = {
   init: function(currentPath) {
 
     this.currentPath = currentPath;
-    var meteorUrl = __MeteorRiderConfig__.meteorUrl;
+    this.meteorUrl = __MeteorRiderConfig__.meteorUrl;
 
-    if (! (meteorUrl.length > 0)) {
+    if (!this.meteorUrl.length > 0) {
       console.error('MeteorRider: error: unable to determine config.meteorUrl');
       return false;
     }
 
-    // non zepto/jquery request
+    // native javascript ajax request
     request = new XMLHttpRequest();
-    request.open('GET', meteorUrl, true);
-    request.onload = this.onSuccess;
+    request.open('GET', this.meteorUrl, true);
+    request.onload = this.onSuccess.bind(this);
     request.onerror = this.onError;
     request.send();
 
@@ -52,35 +52,35 @@ var MeteorRider = {
 
   onSuccess: function () {
     if (request.status >= 200 && request.status < 400) {
-      var meteorUrl = __MeteorRiderConfig__.meteorUrl;
+
       var data = request.responseText;
 
       console.log("MeteorRider success");
-      console.log(data);
+
       // update URLs to include domain prefix
-      data = data.replace(/(href|src|manifest)\=\"\//gm, '$1="' + meteorUrl + '/');
-      console.log(meteorUrl);
-      console.log(data);
+      data = data.replace(/(href|src|manifest)\=\"\//gm, '$1="' + this.meteorUrl + '/');
 
       // set 'currentPath' to empty string if not passed
       this.currentPath = (typeof this.currentPath === 'string' ? this.currentPath : '');
+
       // set the window.location object correctly so iron-router
       // and other packages that depend on window.location work correctly
       if (typeof window.history.replaceState === 'function') {
         // window.history.replaceState() not supported in all clients
-        window.history.replaceState({}, "", meteorUrl + this.currentPath);
+        window.history.replaceState({}, "", this.meteorUrl + this.currentPath);
       } else {
         // TODO: should we do window.history.add() or something?
       }
 
       // replace the document with the new document/data
-      //   this is the REAL hijacking...
-      //     all old JS remains (unless overwritten, name collision)
-      //     all HTML is replaced/overwritten
-      //     all new CSS/JS is loaded
+      // this is the REAL hijacking...
+      //   all old JS remains (unless overwritten, name collision)
+      //   all HTML is replaced/overwritten
+      //   all new CSS/JS is loaded
       document.open();
       document.write(data);
       document.close();
+
       // trigger the "loaded" events (it'd be nice to do this AFTER JS has loaded
       function triggerEvent (eventName) {
         var event = document.createEvent('Event');
